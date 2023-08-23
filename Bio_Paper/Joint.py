7348040975302db5c9ae82e1a0138026603c2610
Almondo4 <77505319+Almondo4@users.noreply.github.com>
2023-08-23 11:26:12 +0300

    finished proposed strat results

--- Bio_Paper/Joint.py
+++ Bio_Paper/Joint.py
@@ -1,3 +1,4 @@
+import yaml
 from avalanche.evaluation.metrics import accuracy_metrics, loss_metrics, timing_metrics, forgetting_metrics, \
     cpu_usage_metrics, disk_usage_metrics, EpochCPUUsage, EpochTime
 from avalanche.logging import TensorboardLogger, TextLogger, InteractiveLogger
@@ -41,7 +42,7 @@ cl_strategy = JointTraining(
     model=model,
     optimizer=torch.optim.Adam(model.parameters(), lr=1e-3),
     criterion=CrossEntropyLoss(),
-    train_mb_size=500, train_epochs=20, eval_mb_size=100,
+    train_mb_size=500, train_epochs=600, eval_mb_size=100,
     evaluator=eval_plugin,
 
 )
@@ -54,15 +55,32 @@ classes_exp = []
 cumulative_set = 0
 
 
-
 #Training for joint training
 try:
     print('benchmark_Joint.train_stream: ', benchmark.train_stream.__len__())
     res = cl_strategy.train(benchmark.train_stream, )
 except Exception as e:
     print(e)
+# Teting for each particular class
 eval_res = cl_strategy.eval(benchmark.test_stream)
+testing_accuracy = []
+for i in range(30):
+    testing_accuracy.append(eval_res[f'Top1_Acc_Exp/eval_phase/test_stream/Task000/Exp{i:03d}'])
+print('Testing accuracy for each class: ', testing_accuracy)
+#testing for total set
+with torch.no_grad():
+    y_pred_logits = cl_strategy.model(torch.tensor(cumulative_benchmark[-1].X, dtype=torch.float32))
+    y_pred_labels = torch.argmax(y_pred_logits, dim=1)
+unique_classes = torch.unique(cumulative_benchmark[-1].targets)
+accuracy = Accuracy(task='multiclass', num_classes=31)
+acc = accuracy(y_pred_labels, cumulative_benchmark[-1].targets)
+print('Accuracy on cumulative_experience: ', acc)
+accuracy.reset()
+#reset accuracy
+accuracy.reset()
 
+
+# Cumulative Testing
 cumulative_test_results = {}
 for set in cumulative_benchmark:
     with torch.no_grad():
@@ -79,4 +97,10 @@ for set in cumulative_benchmark:
     # ad to dict cumulative_final_results
     cumulative_test_results['Cumulative Set ' + str(cumulative_benchmark.index(set))] = acc.item()
 
-# TODO get accuracy  after checking the outcome of the joint training
+# TODO: Save the results in a dictionary and save it in a yaml file
+all_results = {'Average Accuracy': acc,
+               'Comprehensive accuracy': testing_accuracy,
+               'Cumulative Test Results': cumulative_test_results,
+               }
+with open('Joint_600_results.yaml', 'w') as file:
+    yaml.dump(all_results, file)
